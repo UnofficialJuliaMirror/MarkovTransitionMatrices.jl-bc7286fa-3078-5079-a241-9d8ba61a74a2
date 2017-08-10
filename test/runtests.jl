f57e742a96ebf88dd7cb649477d5a6a69204eb58
Mark Agerton (home) <magerton@rice.edu>
2017-08-09 21:40:42 -0500

    First commit. Tests are running

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,30 @@
 using MarkovTransitionMatrices
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+using Distributions
+
+isapprox_oneval(x::AbstractArray) = all( x .≈ x[1] )
+
+# setup for test
+testvec = -3.0:0.5:0.0
+ss = collect(Base.product(testvec, testvec))
+n = length(testvec)
+nn = length(ss)
+mvd = MvNormal(zeros(2), eye(2))
+
+# test the 1-d version
+fullp1 = full(markov_transition((s) -> 0., (s) -> 1., 1e-8, testvec))
+ratio1 = fullp1 ./ pdf(Normal(0,1), testvec)
+@test isapprox_oneval(ratio1)
+
+# test the 2-d version
+fullp2 = full(markov_transition((s) -> zeros(2), (s) -> eye(2), 1e-8, testvec, testvec))
+ratio2 = reshape(fullp2[:,1], n, n) ./  [pdf(mvd, [s...]) for s in ss]
+@test isapprox_oneval(ratio2)
+
+# test the markov-switching version
+fullp3 = full(markovswitching_transition((r,s) -> r==1 ? zeros(2) : [s...], (r,s) -> r == 1 ? eye(2) : eps(1.)*eye(2), eye(Float64, 2), 0.0, testvec, testvec))
+@test all(fullp3[1:nn, 1:nn] .== fullp2)
+@test all(fullp3[nn+1:2*nn, 1:nn] .== 0.0)
+@test all(fullp3[1:nn, nn+1:2*nn] .== 0.0)
+@test all(fullp3[nn+1:2*nn, nn+1:2*nn] .== eye(nn))
