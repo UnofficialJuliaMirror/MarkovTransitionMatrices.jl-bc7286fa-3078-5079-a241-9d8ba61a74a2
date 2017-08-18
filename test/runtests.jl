aff180f7fb8196a03f42b85c63cc8693c64d5041
Mark Agerton (home) <magerton@rice.edu>
2017-08-18 09:32:54 -0500

    stop transposing transition matrices

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,16 +14,20 @@ mvd = MvNormal(zeros(2), eye(2))
 
 # test the 1-d version
 fullp1 = full(markov_transition((s) -> 0., (s) -> 1., 1e-8, testvec))
-ratio1 = fullp1 ./ pdf(Normal(0,1), testvec)
+ratio1 = fullp1 ./ pdf(Normal(0,1), testvec')
 @test isapprox_oneval(ratio1)
 
 # test the 2-d version
 fullp2 = full(markov_transition((s) -> zeros(2), (s) -> eye(2), 1e-8, testvec, testvec))
-ratio2 = reshape(fullp2[:,1], n, n) ./  [pdf(mvd, [s...]) for s in ss]
+ratio2 = reshape(fullp2[1,:], n, n) ./  [pdf(mvd, [s...]) for s in ss]
 @test isapprox_oneval(ratio2)
 
 # test the markov-switching version
-fullp3 = full(markovswitching_transition((r,s) -> r==1 ? zeros(2) : [s...], (r,s) -> r == 1 ? eye(2) : eps(1.)*eye(2), eye(Float64, 2), 0.0, testvec, testvec))
+μswitch(r::Real,s::NTuple{N,T}) where {N,T<:Real} = r==1 ? zeros(2) : [s...]
+Σswitch(r::Real,s::NTuple{N,T}) where {N,T<:Real} = r==1 ? eye(2)   : eps(1.)*eye(2)
+πswitch = eye(Float64, 2)
+fullp3 = full(markovswitching_transition(μswitch, Σswitch, πswitch, 0.0, testvec, testvec))
+
 @test all(fullp3[1:nn, 1:nn] .== fullp2)
 @test all(fullp3[nn+1:2*nn, 1:nn] .== 0.0)
 @test all(fullp3[1:nn, nn+1:2*nn] .== 0.0)
